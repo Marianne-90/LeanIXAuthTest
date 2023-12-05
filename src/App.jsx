e27fda4e80b729b6921a79b94d316ade48f19eac
Marianne <marianne.garridom@gmail.com>
2023-12-04 18:21:13 -0600

    ctrlz

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [base64, setBase64] = useState("");
-  const [accessTokenData, setAccessTokenData] = useState("");
+
+const [base64, setBase64] = useState("");
+
+
 
   const handleOnAccess = () => {
     const username = "apitoken";
@@ -14,67 +16,45 @@ function App() {
     const encodedCredentials = btoa(credentials);
     setBase64(encodedCredentials);
     console.log(encodedCredentials);
+    
+
   };
 
   const handleOnClic = async () => {
     // base64Credentials is the base64 encoded string you generated in Step 2.1
     const base64Credentials = base64;
     var instance = "https://demo-us.leanix.net";
-    const tokenEndpoint =
-      "https://demo-us.leanix.net/services/mtm/v1/oauth2/token";
-
+    const tokenEndpoint = "https://demo-us.leanix.net/services/mtm/v1/oauth2/token"
+    
     // Step 2: Make a POST request to obtain the access token
-    try {
-      const response = await axios.post(
-        tokenEndpoint,
-        "grant_type=client_credentials",
-        {
-          headers: {
-            Authorization: `Basic ${base64Credentials}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-
-      // Step 3: Receive and use the access token
-      const accessToken = response.data.access_token;
-      setAccessTokenData(accessToken);
-      console.log("Access Token:", accessToken);
-
-      // Now you can use the accessToken in your API requests
-    } catch (error) {
-      console.error("Error obtaining access token:", error.message);
-    }
-  };
-
-  const handleAuthentication = () => {
-    // Replace {SUBDOMAIN} with your actual subdomain
-    const url = "https://demo-us.leanix.net/demo/api/v1/services";
-    const accessToken = accessTokenData; // Replace with your actual access token
-
-    // Step 2: Make a GET request to the /v1/services API endpoint
-    axios
-      .get(url, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((response) => {
-        // Process the response as needed
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error:", error.message);
-      });
+      try {
+        const response = await axios.post(
+          tokenEndpoint,
+          'grant_type=client_credentials',
+          {
+            headers: {
+              Authorization: `Basic ${base64Credentials}`,
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          }
+        );
+    
+        // Step 3: Receive and use the access token
+        const accessToken = response.data.access_token;
+        console.log('Access Token:', accessToken);
+    
+        // Now you can use the accessToken in your API requests
+      } catch (error) {
+        console.error('Error obtaining access token:', error.message);
+      }
+    
+    
   };
 
   return (
     <>
-      <button onClick={handleOnAccess}>Obtain an Access Token</button>
-      <button onClick={handleOnClic}>
-        Make Authenticated Requests to Services
-      </button>
-      <button onClick={handleAuthentication}>Authentication</button>
+     <button onClick={handleOnAccess}>Obtain an Access Token</button>
+      <button onClick={handleOnClic}>Make Authenticated Requests to Services</button>
     </>
   );
 }
